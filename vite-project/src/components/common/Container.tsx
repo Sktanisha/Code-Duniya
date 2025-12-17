bd33de0cf736f4abc638bb594c9dee4e1bb6f5d4
Sktanisha <sanjidatanisha99@gmail.com>
2025-12-17 14:02:59 -0500

    nav and banner

---
+++ vite-project/src/components/common/Container.tsx
@@ -0,0 +1,13 @@
+import type React from "react"
+
+
+const Container = ({children} : {children: React.ReactNode}) => {
+  return (
+      <div className="max-w-[1180px] mx-auto">
+     {children}
+    </div>
+    
+  )
+}
+
+export default Container
